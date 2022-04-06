e6c4e8c29c870ad83a1fe2786b1e3c841f8a593d
KaioGalvao <kaioninin@hotmail.com>
2022-04-06 08:40:14 -0300

    Versão do dotnet e Program.cs  atualizada

--- Program.cs
+++ Program.cs
@@ -1,30 +1,17 @@
-﻿using System;
+﻿Console.WriteLine("\t\t\tCALCULADORA IMC\n-----------------------------------------------------------------");
 
-namespace CalculadoraIMC
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {   
-            Console.WriteLine("\t\t\tCALCULADORA IMC\n-----------------------------------------------------------------");
+Console.WriteLine("Qual o seu nome?");
+string nome = Console.ReadLine();
 
-            Console.WriteLine("Qual o seu nome?");
-            string nome = Console.ReadLine();
+Console.WriteLine("Qual a sua altura em metros? Ex: 1,70 ");
+float altura = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Qual a sua altura em metros? Ex: 1,70 ");
-            float altura = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Qual o seu peso em Kg? Ex: 54,3");
+float peso = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Qual o seu peso em Kg? Ex: 54,3");
-            float peso = Convert.ToSingle(Console.ReadLine());
+double imc = peso / (altura * altura);
 
-            double imc = peso / (altura * altura);
+Console.WriteLine("{0}, você tem {1} metros de altura e {2} Kg.", nome, altura, peso);
 
-            Console.WriteLine("{0}, você tem {1} metros de altura e {2} Kg.", nome, altura, peso);
-
-            Console.WriteLine("Seu imc é {0}", imc);
-            Console.WriteLine("------------------------------------------------------------------");
-
-
-        }
-    }
-}
+Console.WriteLine("Seu imc é {0}", imc);
+Console.WriteLine("------------------------------------------------------------------");

6d617cca9b0f8aa31e46f7548183de229825e320
KaioGalvao <kaioninin@hotmail.com>
2022-04-06 08:54:03 -0300

    Adicionado tabela de IMC

--- Program.cs
+++ Program.cs
@@ -15,3 +15,31 @@ Console.WriteLine("{0}, você tem {1} metros de altura e {2} Kg.", nome, altura,
 
 Console.WriteLine("Seu imc é {0}", imc);
 Console.WriteLine("------------------------------------------------------------------");
+
+if (imc < 17)
+{    
+    Console.WriteLine("Muito abaixo do peso");
+}
+else if( imc < 18.49 && imc > 17){
+    Console.WriteLine("Abaixo do peso");
+}
+else if( imc > 18.5 && imc < 24.99)
+{
+    Console.WriteLine("Peso normal");
+}
+else if( imc > 25 && imc < 29.99)
+{
+    Console.WriteLine("Acima do peso");
+}
+else if( imc > 30 && imc < 34.99)
+{
+    Console.WriteLine("Obesidade I");
+}
+else if( imc > 35 && imc < 39.99)
+{
+    Console.WriteLine("Obesidade II");
+}
+else
+{
+    Console.WriteLine("Obesidade III (mórbida)");
+}

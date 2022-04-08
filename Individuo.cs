48161ebae25d85c806abc46f95f4ef8b8721e05b
KaioGalvao <kaioninin@hotmail.com>
2022-04-08 19:25:04 -0300

    Adicionado na classe Individuo o método que calcula quanto peso a pessoa tem que ganhar ou perde para atingir o IMC ideal.

--- Individuo.cs
+++ Individuo.cs
@@ -12,5 +12,48 @@ public class Individuo
     }
 
     public double CalculoIMC(float altura, float peso) => peso / (altura * altura);
-    
+
+
+     public String toString(double imc)
+    {
+        return Nome + " sua altura é " + Math.Round(Altura, 2) + " e seu peso é " + Math.Round(Peso, 2) + ". \nSeu IMC é " + Math.Round(imc, 2) + ".";
+    }
+
+    public String ClassificacaoIMC(double imc)
+    {
+        if (imc < 17) return "Muito abaixo do peso.";
+        
+        else if( imc < 18.49 && imc > 17) return "Abaixo do peso.";
+        
+        else if( imc > 18.5 && imc < 24.99) return "Peso normal.";
+        
+        else if( imc > 25 && imc < 29.99) return "Acima do peso.";
+        
+        else if( imc > 30 && imc < 34.99) return "Obesidade I.";
+        
+        else if( imc > 35 && imc < 39.99) return "Obesidade II.";
+        
+        return "Obesidade III (mórbida).";
+    }
+
+    public String PesoIdeal(float altura, float peso, double imc)
+    {
+        double pesoIdeal = 0.0;
+
+        if (imc < 18.5)
+        {
+            pesoIdeal = (18.5 * altura* altura) - peso;
+
+            return "Para o peso ideal, é necessário ganhar " + Math.Round(pesoIdeal,2) + "Kg.";
+        }
+
+        else if(imc > 24.9)
+        {
+            pesoIdeal = peso - (24.9 * altura * altura);
+
+            return "Para o peso ideal, é necessário perder " + Math.Round(pesoIdeal, 2) + " Kg.";
+        }
+
+        return "Não precisa ganhar ou perder peso.";
+    }
 }
--- Program.cs
+++ Program.cs
@@ -13,39 +13,9 @@ Individuo i =  new Individuo(nome, altura, peso);
 
 double imc = i.CalculoIMC(altura, peso);
 
-Console.WriteLine("{0}, você tem {1} metros de altura e {2} Kg.", nome, altura, peso);
-
-Console.WriteLine("Seu imc é {0}", imc);
-Console.WriteLine("------------------------------------------------------------------");
-
-
-
-
-/*
-if (imc < 17)
-{    
-    Console.WriteLine("Muito abaixo do peso");
-}
-else if( imc < 18.49 && imc > 17){
-    Console.WriteLine("Abaixo do peso");
-}
-else if( imc > 18.5 && imc < 24.99)
-{
-    Console.WriteLine("Peso normal");
-}
-else if( imc > 25 && imc < 29.99)
-{
-    Console.WriteLine("Acima do peso");
-}
-else if( imc > 30 && imc < 34.99)
-{
-    Console.WriteLine("Obesidade I");
-}
-else if( imc > 35 && imc < 39.99)
-{
-    Console.WriteLine("Obesidade II");
-}
-else
-{
-    Console.WriteLine("Obesidade III (mórbida)");
-}*/
+Console.WriteLine("\t\t\tPrimeira Parte\n-----------------------------------------------------------------");
+Console.WriteLine(i.toString(imc));
+Console.WriteLine("\t\t\tSegunda Parte\n-----------------------------------------------------------------");
+Console.WriteLine(i.ClassificacaoIMC(imc));
+Console.WriteLine("\t\t\tTerceira Parte\n-----------------------------------------------------------------");
+Console.WriteLine(i.PesoIdeal(altura, peso, imc));
